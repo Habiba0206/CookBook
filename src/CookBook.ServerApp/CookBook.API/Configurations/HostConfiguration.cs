702a4f1f3cafb78870644bac8e44dee10183b2a4
Habiba0206 <habibasattorova26@gmail.com>
2024-02-19 20:14:07 +0500

    Initialized project, created host configuration

---
+++ src/CookBook.ServerApp/CookBook.API/Configurations/HostConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CookBook.API.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder) 
+    {
+        builder
+            .AddExposers()
+            .AddDevTools();
+
+        return new(builder);
+    }
+
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app
+            .UseExposers()
+            .UseDevTools();
+
+        return new(app);
+    }
+}
