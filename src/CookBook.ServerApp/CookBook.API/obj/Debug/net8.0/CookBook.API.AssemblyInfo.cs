a26949366ab0909706924017804aa10681b17da0
Habiba0206 <habibasattorova26@gmail.com>
2024-02-19 20:26:54 +0500

    something

--- src/CookBook.ServerApp/CookBook.API/obj/Debug/net8.0/CookBook.API.AssemblyInfo.cs
+++ src/CookBook.ServerApp/CookBook.API/obj/Debug/net8.0/CookBook.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CookBook.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d71fddd4518609363fa1f9ccce7af172b5dd503")]
 [assembly: System.Reflection.AssemblyProductAttribute("CookBook.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CookBook.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
